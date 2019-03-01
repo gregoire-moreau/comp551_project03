acfd9afa25a2fbea2237dafc1df5eff8b10adf85
gregoire-moreau <gregoire.moreau@student.uclouvain.be>
2019-03-01 14:11:16 -0500

    Adding code to load data

---
+++ load_data.py
@@ -0,0 +1,20 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+import os
+print(os.listdir("./input"))
+
+# Any results you write to the current directory are saved as output
+
+train_images = pd.read_pickle('./input/train_images.pkl')
+train_labels = pd.read_csv('./input/train_labels.csv')
+
+img_idx = 16
+
+plt.title('Label: {}'.format(train_labels.iloc[img_idx]['Category']))
+plt.imshow(train_images[img_idx])
